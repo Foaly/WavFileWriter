bb4773eb67d3df7957eb7b335d5c6cad98b0ccf7
Maximilian Wagenbach <maximilian.wagenbach@native-instruments.de>
2018-06-01 17:49:22 +0200

    First implementation.

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ It nicely abstracts all the details away and offers a clean interface.
 
 The code is inspired by the Recorder example of Paul Stoffregens Audio library and the SFML SoundFileWriterWav class.
 Hence it is also licensed under the zlib license (see LICENSE.md).
+
+
+Note: Right now only mono recording is supported.
---
+++ WavFileWriter.ino
@@ -0,0 +1,76 @@
+#include "WavFileWriter.hpp"
+#include "Utils.hpp"
+
+#include <SerialFlash.h>
+
+#include <Audio.h>
+#include <Wire.h>
+
+const int Fs = 44100;
+
+//const int myInput = AUDIO_INPUT_LINEIN;
+const int myInput = AUDIO_INPUT_MIC;
+
+AudioPlaySdWav           audioSD;
+AudioSynthWaveform       sine;
+AudioInputI2S            audioInput;
+AudioOutputI2S           audioOutput;
+AudioRecordQueue         queue1;
+
+//recod from mic
+AudioConnection          patchCord1(audioInput, 0, queue1, 0);
+//AudioConnection          patchCord1(sine, 0, queue1, 0);
+AudioConnection          patchCord2(audioSD, 0, audioOutput, 0);
+AudioConnection          patchCord3(audioSD, 0, audioOutput, 1);
+
+AudioControlSGTL5000     audioShield;
+
+elapsedMillis  elapsedMs;
+
+WavFileWriter wavWriter(queue1);
+
+void setup() {
+    setI2SFreq(Fs);
+
+    Serial.begin(9600);
+    AudioMemory(60);
+    audioShield.enable();
+    audioShield.inputSelect(myInput);
+    audioShield.micGain(40);  //0-63
+    audioShield.volume(0.7);  //0-1
+
+    sine.begin(WAVEFORM_SINE);
+    sine.amplitude(0.9);
+    sine.frequency(440);
+
+    delay(1000);
+
+    elapsedMs = 0;
+    wavWriter.open("new.wav", Fs, 2);
+
+    Serial.println("Done initilizing! Starting now!");
+}
+
+
+void loop() {
+    /*if (Serial.available() > 0) {
+        // read the incoming byte:
+        byte incomingByte = Serial.read();
+
+        if ( incomingByte == '1' ) {
+            Serial.println("Start recording!");
+            wavWriter.open("second.wav", Fs, 1);
+        }
+        if ( incomingByte == '2' ) {
+            Serial.println("Stop recording!");
+            wavWriter.close();
+        }
+    }*/
+
+    if (elapsedMs > 5000) {
+        wavWriter.close();
+    }
+
+    if (wavWriter.isWriting())
+        wavWriter.update();
+}
